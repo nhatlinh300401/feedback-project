250300057933122f4f3e148164f3e180fdaa5c7c
Mason Nguyen <nhatlinh300401@gmail.com>
2024-09-06 02:23:38 -0700

    Create Context for Feedback App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from './pages/AboutPage';
 import AboutIconLink from './components/AboutIconLink';
+import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -23,24 +24,26 @@ function App() {
   }
 
   return (
-    <Router>
-        <div className="container">
+    <FeedbackProvider>
+      <Router>
+          <div className="container">
 
-          <Routes>
-            <Route exact path='/' element={
-              <>
-                <FeedbackForm handleAdd={addFeedback}/>
-                <FeedbackStats feedbacks={feedback} />
-                <FeedbackList feedbacks={feedback} handleDelete={deleteFeedback}/>
-              </>
-            }></Route>
+            <Routes>
+              <Route exact path='/' element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback}/>
+                  <FeedbackStats feedbacks={feedback} />
+                  <FeedbackList feedbacks={feedback} handleDelete={deleteFeedback}/>
+                </>
+              }></Route>
 
-            <Route path='/about' element={<AboutPage />} />
-          </Routes>
+              <Route path='/about' element={<AboutPage />} />
+            </Routes>
 
-          <AboutIconLink />
-        </div>
-    </Router>
+            <AboutIconLink />
+          </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
