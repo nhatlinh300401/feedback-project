022a6dc931c800683b7cced097f62a38cac9d865
Mason Nguyen <nhatlinh300401@gmail.com>
2024-09-13 14:56:55 -0700

    Adding Data Fetch from JSON server

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,7 @@
+import spinner from '../assets/spinner.gif'
+
+export default function Spinner() {
+  return (
+    <img src={spinner} alt='Loading...' style={{ width: '100px', margin: 'auto', display: 'block' }}/>
+  )
+}
--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,21 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This item is from context',
-            rating: 10,
-        },
-    ])
-
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    const fetchFeedback = async () => {
+        const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await response.json()
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     // Add Feedback
     const addFeedback = (newFeedback) => {
@@ -48,6 +53,7 @@ export const FeedbackProvider = ({children}) => {
             value={{
                 feedback,
                 addFeedback,
+                isLoading,
                 deleteFeedback,
                 editFeedback,
                 feedbackEdit,
